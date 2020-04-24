85832cc1e306d29b3d313947079861b8afda8214
Albert Smit <albertsmit93@gmail.com>
2020-04-24 09:37:08 +0200

    added timeseries data for current data

--- index.js
+++ index.js
@@ -26,6 +26,8 @@ module.exports = function(app) {
     const isoHandler = function(req, res, next) {
       res.type('text/xml');
       updateDataChannelList(plugin)
+      let dataChannelList = createTabularDataChannelList(plugin.dataChannelList.availablePaths, plugin.dataChannelList)
+      let dataset = createDataSets(plugin.dataChannelList.availablePaths, plugin.dataChannelList)
       let data = {
         Package: {
           Header: {
@@ -33,11 +35,17 @@ module.exports = function(app) {
 
           },
           TimeSeriesData: {
+            TabularData: {
+              NumberOfDataSet: 2,
+              NumberOfDataChannel: dataChannelList.length,
+              DataChannelID: dataChannelList,
+              DataSet: dataset
+            }
 
           }
         }
       }
-      res.send(builder.buildObject(data) + '\n')
+      res.send(builder.buildObject(data) + '\n');
     };
     const channelListHandler = function(req, res, next) {
       res.type('text/xml');
@@ -48,13 +56,13 @@ module.exports = function(app) {
             ShipID: app.getSelfPath('mmsi'),
             DataChannelListID: {
               ID: 'signalk/v1/api/iso19848/datachannelist.xml',
-              TimeStamp: plugin.dataChanneList.timeStamp.toISOString().split('.')[0] + "Z"
+              TimeStamp: stripMiliSeconds(plugin.dataChannelList.timeStamp)
             },
             Author: PLUGIN_ID
 
           },
           DataChannelList: {
-            DataChannel: plugin.dataChanneList.availablePaths.map(createDataChannel)
+            DataChannel: plugin.dataChannelList.availablePaths.map(createDataChannel)
           }
         }
       }
@@ -69,13 +77,90 @@ module.exports = function(app) {
     return router;
   };
 
+  function stripMiliSeconds(date) {
+    return date.toISOString().split('.')[0] + "Z"
+  }
+
+  function createDataSets(paths, dataChannelList) {
+    let dataset = {}
+    paths.forEach((path, index) => {
+      let data = app.getSelfPath(path)
+      let value = data.value
+      let time = data.timestamp
+      if (!time) {
+        time = new Date(0).toISOString()
+      }
+      let timestamp = stripMiliSeconds(new Date(time))
+      if (!dataset[timestamp]) {
+        dataset[timestamp] = new Array(paths.length)
+      }
+      dataset[timestamp][getShortID(path, dataChannelList)] = value
+    })
+    let res = Object.entries(dataset).map(([timestamp, values]) => {
+      return createDataSet(timestamp, values)
+    })
+    return res
+  }
+
+  function createDataSet(timestamp, values) {
+    return {
+      $: {
+        timeStamp: timestamp
+      },
+      Value: createValueList(values)
+    }
+  }
+
+  function createValueList(values) {
+    let res = new Array(values.length)
+    for (var i = res.length - 1; i >= 0; i--) {
+      res[i] = createDataSetValue(values[i], i)
+    }
+    return res
+  }
+
+  function createDataSetValue(value, id) {
+    if (value == null) {
+      value = 'Null'
+    }
+    return {
+      $: {
+        ref: id
+      },
+      _: value
+    }
+  }
+
+  function createTabularDataChannelList(paths, dataChannelList) {
+    return paths.map((path, index) => {
+      return createTabularDataChannel(getShortID(path, dataChannelList), index)
+    })
+  }
+
+  function createTabularDataChannel(shortID, index) {
+    return {
+      $: {
+        id: index
+      },
+      _: shortID
+    }
+  }
+
+  function getShortID(path, dataChannelList) {
+    return dataChannelList.availablePaths.indexOf(path);
+  }
+
+  function getPath(shortID, dataChannelList) {
+    return dataChannelList.availablePaths[shortID];
+  }
+
   function updateDataChannelList(plugin) {
     let temp = createDataChannelList()
-    if (!plugin.dataChanneList) {
-      plugin.dataChanneList = temp
+    if (!plugin.dataChannelList) {
+      plugin.dataChannelList = temp
       app.debug('creating new list')
-    } else if (!dataChannelListIsEqual(plugin.dataChanneList, temp)) {
-      plugin.dataChanneList = temp
+    } else if (!dataChannelListIsEqual(plugin.dataChannelList, temp)) {
+      plugin.dataChannelList = temp
       app.debug('updating list')
     }
   }
