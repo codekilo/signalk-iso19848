8cc8d3a9159e0e86bcc615b419346518e11a9db2
Albert Smit <albertsmit93@gmail.com>
2020-04-29 14:46:26 +0200

    moved iso19848 generation to own modules

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const xml2js = require('xml2js');
 var builder = new xml2js.Builder();
 const DataChannelList = require('./lib/DataChannelList.js')
 const util = require('./lib/util.js')
+const isoDataChannelList = require('./lib/iso19848/DataChannelList.js')
+const isoTimeSeries = require('./lib/iso19848/TimeSeries.js')
 module.exports = function(app) {
   var plugin = {};
   plugin.id = PLUGIN_ID;
@@ -27,46 +29,16 @@ module.exports = function(app) {
     const isoHandler = function(req, res, next) {
       res.type('text/xml');
       plugin.dataChannelList.update()
-      let dataChannelList = createTabularDataChannelList(plugin.dataChannelList.getPaths(), plugin.dataChannelList)
-      let dataset = createDataSets(plugin.dataChannelList.availablePaths, plugin.dataChannelList)
-      let data = {
-        Package: {
-          Header: {
-            ShipID: app.getSelfPath('mmsi')
-
-          },
-          TimeSeriesData: {
-            TabularData: {
-              NumberOfDataSet: dataset.length,
-              NumberOfDataChannel: dataChannelList.length,
-              DataChannelID: dataChannelList,
-              DataSet: dataset
-            }
-
-          }
-        }
-      }
+      let dataset = getData(plugin.dataChannelList.getPaths())
+      let mmsi = app.getSelfPath('mmsi')
+      let data = isoTimeSeries.create(mmsi, plugin.dataChannelList.getPaths(), plugin.dataChannelList, dataset)
       res.send(builder.buildObject(data) + '\n');
     };
     const channelListHandler = function(req, res, next) {
       res.type('text/xml');
       plugin.dataChannelList.update()
-      let data = {
-        Package: {
-          Header: {
-            ShipID: app.getSelfPath('mmsi'),
-            DataChannelListID: {
-              ID: 'signalk/v1/api/iso19848/datachannelist.xml',
-              TimeStamp: util.stripMiliSeconds(plugin.dataChannelList.time)
-            },
-            Author: PLUGIN_ID
-
-          },
-          DataChannelList: {
-            DataChannel: plugin.dataChannelList.getDataChannelList()
-          }
-        }
-      }
+      let mmsi = app.getSelfPath('mmsi')
+      let data = isoDataChannelList.create(mmsi, plugin.dataChannelList, PLUGIN_ID)
       res.send(builder.buildObject(data) + '\n')
 
     }
@@ -78,70 +50,20 @@ module.exports = function(app) {
     return router;
   };
 
-
-  function createDataSets(paths, dataChannelList) {
-    let dataset = {}
-    paths.forEach((path, index) => {
-      let data = app.getSelfPath(path.path)
+  function getData(paths) {
+    return paths.map(path => {
+      let data = app.getSelfPath(path)
       let value = data.value
       let time = data.timestamp
       if (!time) {
         time = new Date(0).toISOString()
       }
-      let timestamp = util.stripMiliSeconds(new Date(time))
-      if (!dataset[timestamp]) {
-        dataset[timestamp] = new Array(paths.length)
+      return {
+        path: path,
+        value: value,
+        timestamp: time
       }
-      dataset[timestamp][path.shortID] = value
     })
-    let res = Object.entries(dataset).map(([timestamp, values]) => {
-      return createDataSet(timestamp, values)
-    })
-    return res
-  }
-
-  function createDataSet(timestamp, values) {
-    return {
-      $: {
-        timeStamp: timestamp
-      },
-      Value: createValueList(values)
-    }
-  }
-
-  function createValueList(values) {
-    let res = new Array(values.length)
-    for (var i = res.length; i >= 0; i--) {
-      res[i] = createDataSetValue(values[i], i)
-    }
-    return res
-  }
-
-  function createDataSetValue(value, id) {
-    if (value == null) {
-      value = 'Null'
-    }
-    return {
-      $: {
-        ref: id
-      },
-      _: value
-    }
-  }
-
-  function createTabularDataChannelList(paths, dataChannelList) {
-    return paths.map((path, index) => {
-      return createTabularDataChannel(dataChannelList.getShortID(path), index)
-    })
-  }
-
-  function createTabularDataChannel(shortID, index) {
-    return {
-      $: {
-        id: index
-      },
-      _: shortID
-    }
   }
 
 
